af5ee503731c2b745ebb52b09f16d693308c5b73
Dax0s <dev@dklimavicius.com>
2024-09-16 14:11:01 +0300

    Remove unneeded fields

--- backend/backend.Tests/Controllers/WeatherForecastControllerTest.cs
+++ backend/backend.Tests/Controllers/WeatherForecastControllerTest.cs
@@ -12,8 +12,6 @@ namespace backend.Tests.Controllers;
 [TestSubject(typeof(WeatherForecastController))]
 public class WeatherForecastControllerTest
 { 
-    private readonly ITestOutputHelper output;
-    
     [Fact]
     public void GetWeatherForecastShouldReturnData()
     {
