612b32718f1c843011a4ff2970fa9c730712317a
Dax0s <56531194+Dax0s@users.noreply.github.com>
2024-09-16 14:22:02 +0300

    2 domas  create first test (#5)
    * Add first test and workflow
    
    * Update dotnet.yml to change dir
    
    * Update WeatherForecastController test
    
    * Update dotnet.yml to change dir before every run
    
    * Fix dotnet.yml file
    
    * Remove unneeded fields

---
+++ backend/backend.Tests/Controllers/WeatherForecastControllerTest.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using backend.Controllers;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace backend.Tests.Controllers;
+
+[TestSubject(typeof(WeatherForecastController))]
+public class WeatherForecastControllerTest
+{ 
+    [Fact]
+    public void GetWeatherForecastShouldReturnData()
+    {
+        ILogger<WeatherForecastController> logger = new Logger<WeatherForecastController>(new LoggerFactory());
+        var controller = new WeatherForecastController(logger);
+        
+        controller.ControllerContext.HttpContext = new DefaultHttpContext();
+        
+        var result = controller.Get().Count();
+        Assert.True(result > 0);
+    }
+}
