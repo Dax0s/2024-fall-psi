bb21fd05e1b18f03bf0ec4197de968d74eb66bf8
AiniusG <ainiusgecas2004@gmail.com>
2024-09-17 13:04:22 +0300

    Added editorConfig for backend and prettier for frontEnd, (#6)
    * Added editorConfig for backend and prettier for frontEnd, updated read me to show how to use these formatting command
    
    * Fix typo in readme
    
    ---------
    
    Co-authored-by: Dax0s <dev@dklimavicius.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,27 +6,27 @@ function App() {
   const [weather, setWeather] = useState('');
   const getWeather = async () => {
     try {
-      const tmp = await fetch("http://localhost:5252/weatherforecast")
-      const json = await tmp.json()
-      console.log(json[0])
+      const tmp = await fetch('http://localhost:5252/weatherforecast');
+      const json = await tmp.json();
+      console.log(json[0]);
       setWeather(json[0].summary);
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-  }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
