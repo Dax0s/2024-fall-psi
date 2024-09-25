5113e3ec0d40a23e6d0f51f9f5b8122bf4e857f8
Dax0s <dev@dklimavicius.com>
2024-09-25 23:26:33 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -36,19 +36,6 @@ function HomePage() {
   return (
     <div className="app">
       <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <hr style={{ width: '100%', color: '#61dbfb' }} />
         <div className="games-grid">
           <Link to={'games/reaction-game'} className="grid-item">
             Reaction Game
