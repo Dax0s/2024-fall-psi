1be65ff1e5e3cccfedf34b6ae607dedb318c6380
Dax0s <dev@dklimavicius.com>
2024-09-25 23:45:42 +0300

    Update tests

--- backend/backend.Tests/AimTrainerGame/Controllers/AimTrainerGameControllerTest.cs
+++ backend/backend.Tests/AimTrainerGame/Controllers/AimTrainerGameControllerTest.cs
@@ -1,8 +1,14 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+using System.Linq;
 using backend.AimTrainerGame.Controllers;
+using backend.AimTrainerGame.Models;
 using JetBrains.Annotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Xunit;
 
 namespace backend.Tests.AimTrainerGame.Controllers;
@@ -12,8 +18,33 @@ public class AimTrainerGameControllerTest
 {
 
     [Fact]
-    public void METHOD()
+    public void StartGameShouldReturnNotNull()
     {
+        ILogger<AimTrainerGameController> logger = new Logger<AimTrainerGameController>(new LoggerFactory());
+        var controller = new AimTrainerGameController();
 
+        var result = controller.StartGame(new GameStartRequest(Difficulty.EASY, new Vector2(1920, 1080)));
+        Assert.True(result != null);
+    }
+
+    [Fact]
+    public void StartGameEasyShouldReturn10Dots()
+    {
+        ILogger<AimTrainerGameController> logger = new Logger<AimTrainerGameController>(new LoggerFactory());
+        var controller = new AimTrainerGameController();
+
+        var result = controller.StartGame(new GameStartRequest(Difficulty.EASY, new Vector2(1920, 1080)));
+        Assert.True(((result.Result as OkObjectResult).Value as GameStartResponse).dotInfos.Count == 10);
+    }
+
+
+    [Fact]
+    public void StartGameHardShouldNotReturn10Dots()
+    {
+        ILogger<AimTrainerGameController> logger = new Logger<AimTrainerGameController>(new LoggerFactory());
+        var controller = new AimTrainerGameController();
+
+        var result = controller.StartGame(new GameStartRequest(Difficulty.HARD, new Vector2(1920, 1080)));
+        Assert.True(((result.Result as OkObjectResult).Value as GameStartResponse).dotInfos.Count != 10);
     }
 }
