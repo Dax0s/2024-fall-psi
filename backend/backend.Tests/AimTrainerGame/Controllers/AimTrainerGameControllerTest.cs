a0373eddf12eecd770980836834f5caefc47f98a
Dax0s <dev@dklimavicius.com>
2024-09-27 15:36:14 +0300

    Clean up project

--- backend/backend.Tests/AimTrainerGame/Controllers/AimTrainerGameControllerTest.cs
+++ backend/backend.Tests/AimTrainerGame/Controllers/AimTrainerGameControllerTest.cs
@@ -1,15 +1,9 @@
-// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-using System;
-using System.Linq;
 using backend.AimTrainerGame.Controllers;
 using backend.AimTrainerGame.Models;
+using backend.Utils;
 using JetBrains.Annotations;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using Xunit;
 
 namespace backend.Tests.AimTrainerGame.Controllers;
