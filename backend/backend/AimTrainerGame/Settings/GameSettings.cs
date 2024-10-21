07b2da6efbd8b6148ffe4fc61954942a12f01d5f
AidasJ21 <aidas.jurelevicius@gmail.com>
2024-10-21 19:17:02 +0300

    Changed order of difficulty settings

--- backend/backend/AimTrainerGame/Settings/GameSettings.cs
+++ backend/backend/AimTrainerGame/Settings/GameSettings.cs
@@ -14,8 +14,8 @@ public static class GameSettings
     }
 
     // Might be a good idea to make public in the future
+    private static readonly DifficultySettings _defaultSettings = new(dotCount: 15, timeToLive: 1250, spawnTime: new(250, 1250));
     private static readonly DifficultySettings _easySettings = new(dotCount: 10, timeToLive: 1500, spawnTime: new(500, 1500));
     private static readonly DifficultySettings _mediumSettings = new(dotCount: 15, timeToLive: 1250, spawnTime: new(250, 1250));
     private static readonly DifficultySettings _hardSettings = new(dotCount: 20, timeToLive: 1000, spawnTime: new(0, 1000));
-    private static readonly DifficultySettings _defaultSettings = new(dotCount: 15, timeToLive: 1250, spawnTime: new(250, 1250));
 }
