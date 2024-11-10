def9df0827444fb68dd675696669482eeb9e6fbc
Dax0s <dev@dklimavicius.com>
2024-11-10 15:24:56 +0200

    Add backend methods for highscore saving

--- backend/backend/AimTrainerGame/Controllers/AimTrainerGameController.cs
+++ backend/backend/AimTrainerGame/Controllers/AimTrainerGameController.cs
@@ -1,6 +1,7 @@
 using backend.AimTrainerGame.Models;
 using backend.AimTrainerGame.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.AimTrainerGame.Controllers;
 
@@ -9,10 +10,12 @@ namespace backend.AimTrainerGame.Controllers;
 public class AimTrainerGameController : ControllerBase
 {
     private readonly IAimTrainerGameService _service;
+    private readonly AimTrainerGameHighscoreContext _db;
 
-    public AimTrainerGameController(IAimTrainerGameService service)
+    public AimTrainerGameController(IAimTrainerGameService service, AimTrainerGameHighscoreContext db)
     {
         _service = service;
+        _db = db;
     }
 
     [HttpPost("StartGame")]
@@ -26,5 +29,29 @@ public class AimTrainerGameController : ControllerBase
         var (dots, difficultySettings) = _service.StartGame(gameInfo);
 
         return Ok(new GameStartResponse(dots, difficultySettings.dotCount, difficultySettings.timeToLive));
+    }
+
+    [HttpGet("Highscores")]
+    public ActionResult<IEnumerable<Highscore>> GetHighscores([FromQuery] int amount = 10)
+    {
+        Console.WriteLine(amount);
+        var highscores = _db.Highscores
+            .OrderBy(h => h.Score)
+            .Take(amount);
+
+        return Ok(highscores);
+    }
+
+    [HttpPost("Highscores")]
+    public ActionResult<Highscore> EndGame([FromBody] GameEndRequest gameInfo)
+    {
+        var hs = new Highscore
+        {
+            Id = Guid.NewGuid(), Username = gameInfo.Username, Score = gameInfo.Score, Date = DateTime.UtcNow
+        };
+        _db.Add(hs);
+        _db.SaveChanges();
+
+        return Ok(hs);
     }
 }
