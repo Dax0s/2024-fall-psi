696a2541737c0d1c0c088d3bf33b2f24daf1f48e
AiniusG <ainiusgecas2004@gmail.com>
2024-12-09 17:31:54 +0200

    Format

--- backend/testing/MathGame/MathGameServiceTest.cs
+++ backend/testing/MathGame/MathGameServiceTest.cs
@@ -59,7 +59,7 @@ public class MathGameServiceTests : IAsyncLifetime
         _context.Puzzles.Add(new Puzzle { Content = "1 + 1" });
         await _context.SaveChangesAsync().ConfigureAwait(true);
 
-        var puzzle = await _service.GetNextPuzzleAsync();
+        var puzzle = await _service.GetNextPuzzleAsync().ConfigureAwait(true);
 
         Assert.NotNull(puzzle);
         Assert.Equal("1 + 1", puzzle);
